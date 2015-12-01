b90b8322d3f6986306676c4a398e87c0cafa51a0
stephan <railsc0d0r@gmail.com>
2015-12-01 07:23:13 +0100

    Adds controller-functionality to login and logout. Refs #54

---
+++ app/controllers/application.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+
+  actions: {
+    invalidateSession() {
+      this.controllerFor('messages').send('loggingOut');
+
+      var _this = this;
+      setTimeout(function(){
+        _this.get('session').invalidate();
+      },2300);
+    }
+  }
+
+});
---
+++ app/routes/application.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+
+export default Ember.Route.extend(ApplicationRouteMixin,{
+  init: function() {
+    const _this = this;
+
+    this.get('session').on('authenticationSucceeded', function() {
+      _this._populateCurrentUser();
+      _this._cleanUpAndTransition();
+      _this.refresh();
+    });
+
+    this._super.apply(this, arguments);
+  },
+
+  afterModel() {
+    let session = this.get('session');
+
+    if (session.get('isAuthenticated')) {
+      this._populateCurrentUser();
+    }
+  },
+
+  _cleanUpAndTransition() {
+    $.modal.close();
+    this.transitionTo('application');
+    this.controllerFor('messages').send('successfulAuthenticated');
+  },
+
+  _populateCurrentUser() {
+    const user_id = this._getUserIdFromSession();
+    return this.store.find('user', user_id).then(user => this.get('currentUser').set('content', user) && user);
+  },
+
+  _getUserIdFromSession() {
+    const user_id = this.get('session.session.content.authenticated.user_id');
+    return user_id;
+  }
+
+});
