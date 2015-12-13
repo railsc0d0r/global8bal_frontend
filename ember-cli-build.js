27391a3e97ff28d92568213ed01946c5c7583431
stephan <railsc0d0r@gmail.com>
2015-12-13 17:39:34 +0100

    Adds lodash to build-script.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,10 +19,13 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/lodash/dist/lodash.js');
+
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/css/bootstrap.css.map');
   app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
   app.import('vendor/styles/all_my_styles.css');
 
   return app.toTree();
