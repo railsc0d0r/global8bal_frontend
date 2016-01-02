21a96831dd2433eaf8d64fdc81d49c07f07cda7c
stephan <railsc0d0r@gmail.com>
2016-01-02 17:08:31 +0100

    Fixes translation-helper. Refs #73

--- app/helpers/i18n_t.js
+++ app/helpers/i18n_t.js
@@ -1,13 +1,8 @@
 import Ember from 'ember';
 
 export  function i18nT(property, options) {
-  var params = options.hash,
+  let params = options.hash,
       self = this;
-
-  // Support variable interpolation for our string
-  Object.keys(params).forEach(function (key) {
-    params[key] = Em.Handlebars.get(self, params[key], options);
-  });
 
   return I18n.t(property, params);
 }
