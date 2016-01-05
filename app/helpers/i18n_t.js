d067f9172b6d102e1a509b92d06eee022815cdf3
stephan <railsc0d0r@gmail.com>
2016-01-05 01:21:45 +0100

    Refactors translation-helper and removes application-view-class. This way there is no need to rerender the whole app when changing the locale. Refs #73

--- app/helpers/i18n_t.js
+++ app/helpers/i18n_t.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
-export  function i18nT(property, options) {
-  let params = options.hash,
-      self = this;
-
-  return I18n.t(property, params);
-}
-
-export default Ember.Handlebars.makeBoundHelper(i18nT);
+export default Ember.Helper.extend({
+  onLocaleChange: Ember.observer('I18n.locale', function() {
+                    this.recompute();
+                  }),
+  compute(params, hash) {
+    return I18n.t(params[0], hash);
+  }
+});
--- app/views/application.coffee
+++
@@ -1,9 +0,0 @@
-`import Ember from 'ember'`
-
-ApplicationView = Ember.View.extend(
-  rerenderApp: (->
-        this.rerender()
-  ).observes('I18n.locale')
-)
-
-`export default ApplicationView`
