adaead11e1e5c3929d9625d2f713ac629cc34601
stephan <railsc0d0r@gmail.com>
2016-03-05 23:49:41 +0100

    Imports lodash where needed. Refs #91

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
--- app/services/handle-errors.coffee
+++ app/services/handle-errors.coffee
@@ -1,4 +1,5 @@
 `import Ember from 'ember'`
+`import _ from 'lodash/lodash'`
 
 HandleErrorsService = Ember.Service.extend(
   joinErrorMessages: (errors) ->
